3c7e33a33247f7fc54dfc46da562e917613dcccb
arthursiq5 <arthur.siq5@gmail.com>
2022-07-09 13:53:33 -0300

    Adicionado exemplo de uso de tipos de dados

---
+++ Basico/tiposDeDados.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let tipo1 = 'c';
+    let tipo2 = "Linguagem Rust";
+    let tipo3 = 1234;
+    let tipo4 = 3.14;
+    let tipo5 = true;
+
+    println!("Dado do tipo char é {}", tipo1);
+    println!("Dado do tipo string é {}", tipo2);
+    println!("Dado do tipo int é {}", tipo3);
+    println!("Dado do tipo float é {}", tipo4);
+    println!("Dado do tipo boolean é {}", tipo5);
+}
+
