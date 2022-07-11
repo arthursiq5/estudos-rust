deba17f9b13313d43cb196bbcdb99f69c602cc7e
arthursiq5 <arthur.siq5@gmail.com>
2022-07-11 13:11:54 -0300

    Adicionado exemplo de alteração de variável

---
+++ Basico/exemploVariavelMutavel.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Precisa ter o modificador "mut" para poder alterar uma variável
+    let mut a_number = 10; 
+    println!("The number is {}.", a_number);
+
+    a_number = 15;
+    println!("Now the number is {}.", a_number);
+}
