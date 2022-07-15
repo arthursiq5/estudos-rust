be19c72fd69957a66fb417a28804cabecce9d4f0
arthursiq5 <arthur.siq5@gmail.com>
2022-07-14 23:22:20 -0300

    Adicionada validação

--- Basico/guessing_game/src/main.rs
+++ Basico/guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use rand::Rng;
 use std::io;
+use std::cmp::Ordering;
 
 fn main() {
     let numero_secreto = rand::thread_rng().gen_range(1..=100);
@@ -14,6 +15,14 @@ fn main() {
     io::stdin()
         .read_line(&mut palpite)
         .expect("Failed to read line");
+
+    let palpite: u32 = palpite.trim().parse().expect("Por favor insira um número!");
     
     println!("Seu palpite é {palpite}");
+
+    match palpite.cmp(&numero_secreto) {
+        Ordering::Less => println!("Muito pequeno!"),
+        Ordering::Greater => println!("Muito grande!"),
+        Ordering::Equal => println!("Você venceu"),
+    }
 }
