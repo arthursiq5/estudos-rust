568a97d710154871627ea31176ab6263374e2e1e
arthursiq5 <arthur.siq5@gmail.com>
2022-07-19 22:10:14 -0300

    Exemplo de uso de print

---
+++ Basico/exemplosComPrint.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello World"); // escreve o texto "Hello World"
+    println!(); // deixa uma linha em branco
+    println!("Exemplo de {} de argumentos {}", "uso", 2+3);
+
+    /*
+    Exemplo de comentário multilinha
+    */
+}
